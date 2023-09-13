c2981b862b29b8aaf51b1f624db9c977e90bed4a
June Castillote <june.castillote@gmail.com>
2023-09-13 10:35:07 +0800

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace DontLetMeSleep
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.StartStopCheckBox = new System.Windows.Forms.CheckBox();
+            this.CustomIntervalTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.StatusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.ToolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.ToolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.StatusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // StartStopCheckBox
+            // 
+            this.StartStopCheckBox.Appearance = System.Windows.Forms.Appearance.Button;
+            this.StartStopCheckBox.AutoSize = true;
+            this.StartStopCheckBox.BackColor = System.Drawing.Color.Transparent;
+            this.StartStopCheckBox.Image = global::DontLetMeSleep.Properties.Resources.START_BUTTON;
+            this.StartStopCheckBox.Location = new System.Drawing.Point(12, 12);
+            this.StartStopCheckBox.Name = "StartStopCheckBox";
+            this.StartStopCheckBox.Size = new System.Drawing.Size(134, 134);
+            this.StartStopCheckBox.TabIndex = 0;
+            this.StartStopCheckBox.UseVisualStyleBackColor = false;
+            this.StartStopCheckBox.CheckedChanged += new System.EventHandler(this.StartStopCheckBox_CheckedChanged);
+            // 
+            // CustomIntervalTextBox
+            // 
+            this.CustomIntervalTextBox.Location = new System.Drawing.Point(104, 157);
+            this.CustomIntervalTextBox.Name = "CustomIntervalTextBox";
+            this.CustomIntervalTextBox.Size = new System.Drawing.Size(42, 20);
+            this.CustomIntervalTextBox.TabIndex = 3;
+            this.CustomIntervalTextBox.Text = "30";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 160);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Interval (seconds)";
+            // 
+            // StatusStrip1
+            // 
+            this.StatusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ToolStripProgressBar1,
+            this.ToolStripStatusLabel1});
+            this.StatusStrip1.Location = new System.Drawing.Point(0, 188);
+            this.StatusStrip1.Name = "StatusStrip1";
+            this.StatusStrip1.Size = new System.Drawing.Size(157, 22);
+            this.StatusStrip1.TabIndex = 6;
+            this.StatusStrip1.Text = "statusStrip1";
+            // 
+            // ToolStripProgressBar1
+            // 
+            this.ToolStripProgressBar1.Name = "ToolStripProgressBar1";
+            this.ToolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            // 
+            // ToolStripStatusLabel1
+            // 
+            this.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1";
+            this.ToolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(157, 210);
+            this.Controls.Add(this.StatusStrip1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CustomIntervalTextBox);
+            this.Controls.Add(this.StartStopCheckBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Don\'t Let Me Sleep";
+            this.StatusStrip1.ResumeLayout(false);
+            this.StatusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox StartStopCheckBox;
+        private System.Windows.Forms.TextBox CustomIntervalTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.StatusStrip StatusStrip1;
+        private System.Windows.Forms.ToolStripProgressBar ToolStripProgressBar1;
+        private System.Windows.Forms.ToolStripStatusLabel ToolStripStatusLabel1;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,80 @@
+﻿using DontLetMeSleep.Properties;
+using System;
+using System.Windows.Forms;
+
+namespace DontLetMeSleep
+{
+    public partial class MainForm : Form
+    {
+        private bool isSimulationRunning = false;
+        private int intervalSeconds = 30; // Initial interval in seconds
+        private Timer countdownTimer = new Timer();
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            // Initialize the countdown timer
+            countdownTimer.Interval = 1000; // 1 second
+            countdownTimer.Tick += CountdownTimer_Tick;
+
+            // Set the initial value of the custom interval TextBox
+            CustomIntervalTextBox.Text = intervalSeconds.ToString();
+        }
+        private void StartStopCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            isSimulationRunning = StartStopCheckBox.Checked;
+
+            // Disable customIntervalTextBox when the simulation is running
+            CustomIntervalTextBox.Enabled = !isSimulationRunning;
+
+            if (isSimulationRunning)
+            {
+                StartSimulation();
+                StartStopCheckBox.Image = Resources.STOP_BUTTON; // Change the button image to STOP
+            }
+            else
+            {
+                StopSimulation();
+                StartStopCheckBox.Image = Resources.START_BUTTON; // Change the button image to START
+            }
+        }
+
+        private void StartSimulation()
+        {
+            intervalSeconds = int.Parse(CustomIntervalTextBox.Text); // Get interval from TextBox
+            ToolStripProgressBar1.Maximum = int.Parse(CustomIntervalTextBox.Text);
+            countdownTimer.Start();
+        }
+
+        private void StopSimulation()
+        {
+            countdownTimer.Stop();
+            UpdateCountdownLabel(0);
+        }
+
+        private void CountdownTimer_Tick(object sender, EventArgs e)
+        {
+            if (intervalSeconds > 0)
+            {
+                intervalSeconds--;
+                UpdateCountdownLabel(intervalSeconds);
+            }
+            else
+            {
+                // Simulate key press when the countdown reaches 0
+                SendKeys.SendWait("{F16}");
+                intervalSeconds = int.Parse(CustomIntervalTextBox.Text); // Get interval from TextBox
+                UpdateCountdownLabel(intervalSeconds);
+            }
+        }
+
+        private void UpdateCountdownLabel(int seconds)
+        {
+            //CountdownLabel.Text = $"Next F16 key press in {seconds} seconds...";
+            //CountdownLabel.Text = $"Key press in {seconds} seconds...";
+            ToolStripProgressBar1.Value = seconds;
+            ToolStripStatusLabel1.Text = seconds.ToString()+"s";
+        }
+    }
+}
